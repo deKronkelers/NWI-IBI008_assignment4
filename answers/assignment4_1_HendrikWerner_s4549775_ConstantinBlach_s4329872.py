d4ec43846eebccbfaf808ba483cd41b60c21a889
Hendrikto <hendrik.to@gmail.com>
2016-11-19 20:10:48 +0100

    Add titles, x labels, and y labels.

--- answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -15,16 +15,24 @@ synth4 = loadmat("./data/synth4.mat")
 synths = [synth1, synth2, synth3, synth4]
 
 
-def plot_clustering(data, clusters: int):
+def plot_clustering(
+        data, clusters: int,
+        title: str = None,
+        xlabel: str = None,
+        ylabel: str = None
+):
     X = data["X"]
     y = data["y"]
     centroids, labels, inertia = k_means(X, clusters)
     clusterPlot(X, labels, centroids, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.show()
 
 
-for synth in synths:
-    plot_clustering(synth, 4)
+for i, synth in enumerate(synths):
+    plot_clustering(synth, 4, title="Synth {}".format(i + 1), xlabel="attribute 1", ylabel="attribute 2")
 
 # assignment 4.1.2
 
