64b216e76ac543e1f2f1576e57b1518a4274f665
Hendrikto <hendrik.to@gmail.com>
2016-11-19 18:32:26 +0100

    Load data.

--- answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -1,7 +1,13 @@
 # author: Hendrik Werner s4549775
 # author: Constantin Blach s4329872
 
+from scipy.io import loadmat
+
 # assignment 4.1.1
+synth1 = loadmat("./data/synth1.mat")
+synth2 = loadmat("./data/synth2.mat")
+synth3 = loadmat("./data/synth3.mat")
+synth4 = loadmat("./data/synth4.mat")
 
 # assignment 4.1.2
 
