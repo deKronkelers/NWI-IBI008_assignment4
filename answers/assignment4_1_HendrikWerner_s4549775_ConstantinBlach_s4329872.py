82be07434b8cf362acb8445be4227bc079ed2985
Hendrikto <hendrik.to@gmail.com>
2016-11-19 17:45:53 +0100

    Add stubs for answers.

---
+++ answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -0,0 +1,10 @@
+# author: Hendrik Werner s4549775
+# author: Constantin Blach s4329872
+
+# assignment 4.1.1
+
+# assignment 4.1.2
+
+# assignment 4.1.3
+
+# assignment 4.1.4
---
+++ answers/assignment4_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -0,0 +1,4 @@
+# author: Hendrik Werner s4549775
+# author: Constantin Blach s4329872
+
+# assignment 4.2.1
