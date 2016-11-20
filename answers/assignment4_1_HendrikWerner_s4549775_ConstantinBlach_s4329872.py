7fb7d26640e68840d83c831b0382f6d62f7f163e
Hendrikto <hendrik.to@gmail.com>
2016-11-20 17:06:22 +0100

    Add additional type annotations.

--- answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment4_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -21,7 +21,7 @@ synths = [synth1, synth2, synth3, synth4]
 
 
 def plot_clustering(
-        data, clusters: int,
+        data: np.ndarray, clusters: int,
         title: str = None,
         xlabel: str = None,
         ylabel: str = None
@@ -43,7 +43,7 @@ for i, synth in enumerate(synths):
 
 # assignment 4.1.2
 def plot_errors(
-        data, range: range,
+        data: np.ndarray, range: range,
         title: str = None,
         xlabel: str = None,
         ylabel: str = None
